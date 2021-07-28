70e4a2bb3fd64eb893bb4d9e8082d87e94e57859
Kirill <shabad.k@yandex.ru>
2021-07-28 04:26:05 +0300

    Добавлено поле id в Place и PlaceResponse

--- app/src/main/java/ru/kkd/kickmap/Place.kt
+++ app/src/main/java/ru/kkd/kickmap/Place.kt
@@ -2,10 +2,10 @@ package ru.kkd.kickmap
 
 import com.google.android.gms.maps.model.LatLng
 
-data class Place(val name: String, val latLng : LatLng)
+data class Place(val id: Int, val name: String, val latLng : LatLng)
 
-data class PlaceResponse(val name: String, val loc : Geom)
+data class PlaceResponse(val id: Int, val name: String, val loc : Geom)
 
 data class Geom(val lat : Double, val lng : Double)
 
-fun PlaceResponse.toPlace() : Place = Place(name, LatLng(loc.lat, loc.lng))
+fun PlaceResponse.toPlace() : Place = Place(id, name, LatLng(loc.lat, loc.lng))
